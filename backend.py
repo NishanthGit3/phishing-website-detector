ddb02af54159ea22ba671c45c3f507383a42ffaf
VishnupiriyanV <vishnupiriyan086@gmail.com>
2025-03-25 23:40:11 +0530

    Updated backend.py

--- backend.py
+++ backend.py
@@ -7,79 +7,82 @@ import re
 import sys
 from datetime import datetime
 from urllib.parse import urlparse
-
-flags = 0 # For percentage calculation
-Total_Checks = 3
-
-#getting url input
-
 link=input("URL:")
-if "http" in link:
-     pass
-else:
-     print("-please enter an url that contains its protocol, ex: http://, https://")
-     print("-program terminated")
-     sys.exit()
-linkdata = whois.whois(link)
-#print(linkdata)
 
-#creation date and current date, verify if the domain is older than a year
+def safe_check_url(link):
+     flags = 0 # For percentage calculation
+     Total_Checks = 3
 
-def whoisd(flags):
-          linkcd = linkdata["creation_date"]
-          if isinstance(linkcd, list):
-            linkcd = linkcd[0]
-          print(linkcd)
-          linkcd1 = linkcd.date()
-          linkcd3 = linkcd1.year
-          nowdt = datetime.now()
-          nowdt1 = nowdt.date()
-          nowdt2 = nowdt1.year
-          sub = nowdt2 - linkcd3
-          if sub >= 1:
-               return True
-          else:
-               return False
+     #getting url input
+
+
+     
+     linkdata = whois.whois(link)
+     #print(linkdata)
+
+     #creation date and current date, verify if the domain is older than a year
+
+     def whoisd(flags):
+               linkcd = linkdata["creation_date"]
+               if isinstance(linkcd, list):
+                    linkcd = linkcd[0]
+               #print(linkcd)
+               linkcd1 = linkcd.date()
+               linkcd3 = linkcd1.year
+               nowdt = datetime.now()
+               nowdt1 = nowdt.date()
+               nowdt2 = nowdt1.year
+               sub = nowdt2 - linkcd3
+               if sub >= 1:
+                    return True
+               else:
+                    return False
     
-if True and whoisd(flags):
-     flags += 1
-     print("-the domain creation date is older than a year")
-else:
-     print("-the domain creation date is lesser than a year")
+     if True and whoisd(flags):
+          flags += 1
+          #print("-the domain creation date is older than a year")
+     else:
+          #print("-the domain creation date is lesser than a year")
+          pass
 
-#special char and long urls
+     #special char and long urls
 
-def is_long_url(url, max_length=100):
-    return len(url) > max_length
+     def is_long_url(url, max_length=100):
+          return len(url) > max_length
 
-def has_special_characters(url):
-    valid_pattern = r'^[a-zA-Z0-9\-._~:/?#[\]@!$&\'()*+,;=%]*$'
-    return not re.match(valid_pattern, url)
+     def has_special_characters(url):
+          valid_pattern = r'^[a-zA-Z0-9\-._~:/?#[\]@!$&\'()*+,;=%]*$'
+          return not re.match(valid_pattern, url)
 
-def detect_url(durl,flags):
-     if is_long_url(durl) or has_special_characters(durl):
-          return True
+     def detect_url(durl,flags):
+          if is_long_url(durl) or has_special_characters(durl):
+               return True
      
-if True and detect_url(link,flags):
-     print("-a problametic url")
-else:
-     print("-url has no special characters or exceeds the max length 100")
-     flags+=1
-     
-#check for ssl protocol
+     if True and detect_url(link,flags):
+          pass
+          #print("-a problametic url")
+     else:
+          #print("-url has no special characters or exceeds the max length 100")
+          flags+=1
+     #check for ssl protocol
 
-def ssl_check(surl):
-     https_pattern = r'^https:\/\/[a-zA-Z0-9\-._~:/?#[\]@!$&\'()*+,;=%]*$'
-     return re.match(https_pattern, surl)
+     def ssl_check(surl):
+          https_pattern = r'^https:\/\/[a-zA-Z0-9\-._~:/?#[\]@!$&\'()*+,;=%]*$'
+          return re.match(https_pattern, surl)
 
-if True and ssl_check(link):
-     flags += 1
-     print("-uses ssl protocol")
-else:
-     print("-does not use ssl protocol, not safe")
+     if True and ssl_check(link):
+          flags += 1
+          #print("-uses ssl protocol")
+     else:
+          pass
+          #print("-does not use ssl protocol, not safe")
 
-def percentage_calculator(flags1,Total_checks):
-     divides = (flags1/Total_Checks)
-     percentage = int(divides*100)
-     print(f'Overall report % : {percentage}')
-percentage_calculator(flags,Total_Checks)
+     def percentage_calculator(flags1,Total_checks):
+          divides = (flags1/Total_Checks)
+          percentage = int(divides*100)
+          #print(f'Overall report % : {percentage}')
+          return percentage
+     report = percentage_calculator(flags,Total_Checks)
+
+     return report
+
